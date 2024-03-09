67472acd9190af57f76c39f050a8d300fdde6422
arafatah <arafathowlader456@gmail.com>
2024-03-09 22:45:07 +0600

    showproduct

--- src/page/ShowProduct/ShowProduct.jsx
+++ src/page/ShowProduct/ShowProduct.jsx
@@ -30,86 +30,84 @@ const ShowProduct = () => {
       <Helmet>
         <title>AN NOOR | HOME</title>
       </Helmet>
-      <div className=" py-6">
+      <div className="py-6">
         <h1 className="text-2xl font-semibold mb-4">NEW ARRIVALS</h1>
         {memoizedProduct && (
-          <div>
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6   gap-4">
-              {memoizedProduct?.data?.products
-                ?.slice(0, 12)
-                .map((productDetail, index) => (
-                  <div
-                    key={index}
-                    className={`bg-white p-4 !h-[450px] rounded-lg shadow-md col-span-1 relative overflow-hidden ${
-                      index >= 15 ? "hidden" : ""
-                    }`}
-                    style={{
-                      height: "400px",
-                      display: "flex",
-                      flexDirection: "column",
-                    }}
-                  >
-                    <div className="group flex-grow">
-                      <div className="relative overflow-hidden aspect-w-1 aspect-h-1 group-hover:scale-105 transition-transform">
-                        <Link to={`/showProduct/${productDetail?._id}`}>
-                          <img
-                            src={productDetail?.image}
-                            alt={productDetail?.yourName}
-                            className="w-full h-[200px] sm:h-[250px] object-cover rounded-md"
-                          />
-                        </Link>
-                      </div>
-                      <Link
-                        to={`/showProduct/${productDetail?._id}`}
-                        className="text-base text-deep-orange-900 font-semibold 
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6   gap-4">
+            {memoizedProduct?.data?.products
+              ?.slice(0, 12)
+              .map((productDetail, index) => (
+                <div
+                  key={index}
+                  className={`bg-white p-4 !h-[450px] rounded-lg shadow-md col-span-1 relative overflow-hidden ${
+                    index >= 15 ? "hidden" : ""
+                  }`}
+                  style={{
+                    height: "400px",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <div className="group flex-grow">
+                    <div className="relative overflow-hidden aspect-w-1 aspect-h-1 group-hover:scale-105 transition-transform">
+                      <Link to={`/showProduct/${productDetail?._id}`}>
+                        <img
+                          src={productDetail?.image}
+                          alt={productDetail?.yourName}
+                          className="w-full h-[200px] sm:h-[250px] object-cover rounded-md"
+                        />
+                      </Link>
+                    </div>
+                    <Link
+                      to={`/showProduct/${productDetail?._id}`}
+                      className="text-base text-deep-orange-900 font-semibold 
                     hover:text-deep-orange-700 transition duration-300 ease-in-out
                   mt-2 flex justify-center overflow-hidden"
-                        style={{ height: "50px" }}
-                      >
-                        {productDetail?.title}
-                      </Link>
+                      style={{ height: "50px" }}
+                    >
+                      {productDetail?.title}
+                    </Link>
 
-                      <div className="flex justify-center">
-                        <Rating
-                          value={productDetail?.averageRating}
-                          count={5}
-                          size={24}
-                          activeColor="#ffd700"
-                          edit={false}
-                        />
-                      </div>
-
-                      <div className="flex justify-center mb-2">
-                        <h3 className="text-sm font-medium mx-3 text-green-500">
-                          ৳ {productDetail?.price}
-                        </h3>
-                      </div>
-
-                      <div className="flex justify-center mb-2">
-                        <h2
-                          className="text-xs text-black overflow-hidden"
-                          style={{ height: "20px" }}
-                        >
-                          {productDetail?.companyName}
-                        </h2>
-                      </div>
+                    <div className="flex justify-center">
+                      <Rating
+                        value={productDetail?.averageRating}
+                        count={5}
+                        size={24}
+                        activeColor="#ffd700"
+                        edit={false}
+                      />
                     </div>
 
+                    <div className="flex justify-center mb-2">
+                      <h3 className="text-sm font-medium mx-3 text-green-500">
+                        ৳ {productDetail?.price}
+                      </h3>
+                    </div>
 
-              <div className="absolute bottom-0 left-0 right-0">
-                <Link
-                to={`/showProduct/${productDetail?._id}`}
-                  className="text-center py-2 flex justify-center items-center rounded-md bg-blue-400 text-white hover:bg-indigo-700 transition duration-300 ease-in-out"
-                  style={{ marginTop: "auto" }}
-                >
-                  <h1 className="text-sm md:text-xs md:font-semibold">
-                    Show details
-                  </h1>
-                  <FaArrowRightLong className="ml-1" />
-                </Link>
-              </div>
+                    <div className="flex justify-center mb-2">
+                      <h2
+                        className="text-xs text-black overflow-hidden"
+                        style={{ height: "20px" }}
+                      >
+                        {productDetail?.companyName}
+                      </h2>
+                    </div>
 
-            </div>
+                    <div className="absolute bottom-0 left-0 right-0">
+                      <Link
+                        to={`/showProduct/${productDetail?._id}`}
+                        className="text-center py-2 flex justify-center items-center rounded-md bg-blue-400 text-white hover:bg-indigo-700 transition duration-300 ease-in-out"
+                        style={{ marginTop: "auto" }}
+                      >
+                        <h1 className="text-sm md:text-xs md:font-semibold">
+                          Show details
+                        </h1>
+                        <FaArrowRightLong className="ml-1" />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              ))}
           </div>
         )}
 
