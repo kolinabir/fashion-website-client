c31c28d5aec0d21b3b4989ca5968cbda0964c3d4
arafatah <arafathowlader456@gmail.com>
2024-03-09 22:02:29 +0600

    add product route sizes and images problem solve2

--- src/page/AddProduct/AddProduct.jsx
+++ src/page/AddProduct/AddProduct.jsx
@@ -171,19 +171,7 @@ const AddProduct = () => {
                   Price
                 </label>
               </div>
-              {/* <div className="relative z-0 w-full mb-6 group">
-                <input
-                  type="text"
-                  name="size"
-                  id="size"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-700  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                  Size
-                </label>
-              </div> */}
+             
               <div className="relative z-0 w-full mb-6 group">
                 <input
                   type="text"
@@ -210,8 +198,6 @@ const AddProduct = () => {
                   Quantity
                 </label>
               </div>
-            </div>
-            <div className="grid md:grid-cols-2 md:gap-6">
               <div className="relative z-0 w-full mb-6 group">
                 <input
                   type="tel"
@@ -225,6 +211,10 @@ const AddProduct = () => {
                   Description
                 </label>
               </div>
+            </div>
+            
+            <div className="grid md:grid-cols-2 md:gap-6">
+             
               <div className="relative z-0 w-full mb-6 group">
                 <input
                   type="text"
